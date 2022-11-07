0fcf2806479295d7363c4c3e9f336eb7d714e9be
rizki-kurniawan <rizki.kurniawan02@ui.ac.id>
2022-11-08 06:18:32 +0700

    Implement Lexical Analyzer

--- src/LexicalAnalyzer.hs
+++ src/LexicalAnalyzer.hs
@@ -1,6 +1,30 @@
 module LexicalAnalyzer (autoParentheses) where
 
-import Text.Regex.TDFA ()
-
 autoParentheses :: String -> String
-autoParentheses _ = ""
+autoParentheses s = case break (=='=') (dropWhile (==' ') s) of
+    (expr, []) -> formatExpr expr
+    (decl, expr) -> trim decl ++ " = " ++ (formatExpr . dropWhile (==' ') . tail) expr
+
+formatExpr :: String -> String
+formatExpr str = "(" ++ auPe str ++ ")"
+    where 
+        auPe (s:ss) 
+            | s == ' ' = let nextToken = dropWhile (==' ') ss in if null nextToken then [] else ' ' : auPe nextToken
+            | s == '.' = let (inScope, outerScope) = (findInScope 0 [] . dropWhile (==' ')) ss in 
+                ". " ++ formatExpr inScope ++ auPe outerScope
+            | otherwise = s : auPe ss
+        auPe [] = ""
+
+        findInScope :: Int -> String -> String -> (String, String)
+        findInScope _ ins [] = (reverse ins, [])
+        findInScope 0 ins (')':ss) = (reverse ins, ')':ss)
+        findInScope n ins (s:ss) = case s of
+            ')' -> findInScope (n - 1) (')':ins) ss
+            '(' -> findInScope (n + 1) ('(':ins) ss
+            _ -> findInScope n (s:ins) ss
+
+trim :: String -> String
+trim [] = []
+trim (s:ss) = case s of
+    ' ' -> let nextToken = dropWhile (==' ') ss in if null nextToken then [] else ' ' : trim nextToken
+    _ -> s : trim ss
