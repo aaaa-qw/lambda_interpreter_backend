c30d6043f57bebd7e68f961b5e06e50f5779bb3f
rizki-kurniawan <rizki.kurniawan02@ui.ac.id>
2022-12-05 22:17:59 +0700

    Fix evaluator test

--- test/EvaluatorSpec.hs
+++ test/EvaluatorSpec.hs
@@ -4,7 +4,8 @@ module EvaluatorSpec (spec) where
 
     import Test.Hspec ( describe, it, shouldBe, Spec, shouldSatisfy)
     import Evaluator (replaceUnbound, betaReduce, evaluate, EvaluationError(..))
-    import Parser (Expr(..), Program(..), parse)
+    import Parser (parse)
+    import Grammar (Program(..), Expr(..))
     import qualified Data.Map as Map
     import Data.Either (isLeft)
 
@@ -30,7 +31,7 @@ module EvaluatorSpec (spec) where
 
             it "All bounded variable; no definition" $ do
                 replaceUnbound Map.empty (getExpr "(lambda x y z.z(lambda a z.a x y z))")
-                `shouldBe` Fun ["x", "y", "z"] (Id "z" (Fun ["a", "z"] (Id "a" (Id "x" (Id "y" (Id "z" ENoCnt)))) ENoCnt)) ENoCnt
+                `shouldBe` Fun ["x", "y", "z"] (Id "z" (Fun ["a", "x0"] (Id "a" (Id "x" (Id "y" (Id "x0" ENoCnt)))) ENoCnt)) ENoCnt
 
             it "Bounded + Unbounded variable; no definition" $ do
                 replaceUnbound Map.empty (getExpr "(lambda x y.z(lambda a z.a x y z)x a)x y z")
@@ -42,7 +43,7 @@ module EvaluatorSpec (spec) where
 
             it "All bounded variable; with definition" $ do
                 replaceUnbound tableSym (getExpr "(lambda x y z.z(lambda a z.a x y z))")
-                `shouldBe` Fun ["x", "y", "z"] (Id "z" (Fun ["a", "z"] (Id "a" (Id "x" (Id "y" (Id "z" ENoCnt)))) ENoCnt)) ENoCnt
+                `shouldBe` Fun ["x", "y", "z"] (Id "z" (Fun ["a", "x0"] (Id "a" (Id "x" (Id "y" (Id "x0" ENoCnt)))) ENoCnt)) ENoCnt
 
             it "All unbound; all variable already defined in table symbol" $ do
                 replaceUnbound tableSym (getExpr "(a b a)") `shouldBe`
@@ -56,13 +57,20 @@ module EvaluatorSpec (spec) where
                 replaceUnbound tableSym (getExpr "(lambda x.x a c d)") `shouldBe`
                     Fun ["x"] (Id "x" (E (Id "x'0" ENoCnt)
                         (E
-                            (Fun ["x"] (Id "x'1" (Fun ["y"] (Id "x'2" (Id "x" (Id "y" ENoCnt))) ENoCnt)) ENoCnt)
+                            (Fun ["x0"] (Id "x'1" (Fun ["y"] (Id "x'2" (Id "x0" (Id "y" ENoCnt))) ENoCnt)) ENoCnt)
                             (Id "x'3" ENoCnt))) ) ENoCnt
 
             it "Variable in Parameter Lists should not be substitued" $ do
                 replaceUnbound tableSym (getExpr "(lambda a c.a b c)") `shouldBe`
-                    Fun ["a", "c"] (Id "a" (E (Fun ["a", "b"] (Id "a" (Id "b" ENoCnt)) ENoCnt) (Id "c" ENoCnt))) ENoCnt
+                    Fun ["a", "c"] (Id "a" (E (Fun ["x0", "b"] (Id "x0" (Id "b" ENoCnt)) ENoCnt) (Id "c" ENoCnt))) ENoCnt
 
+            it "Complex nested function" $ do
+                replaceUnbound Map.empty (getExpr "(lambda x.(lambda x.(lambda x.(lambda x.x))))") `shouldBe`
+                    Fun ["x"] (Fun ["x0"] (Fun ["x1"] (Fun ["x2"] (Id "x2" ENoCnt) ENoCnt) ENoCnt) ENoCnt) ENoCnt
+            
+            it "Function with duplicate parameter" $ do 
+                replaceUnbound Map.empty (getExpr "(lambda x x y x.x)") `shouldBe`
+                    Fun ["x"] (Fun ["x0"] (Fun ["y"] (Fun ["x1"] (Id "x1" ENoCnt) ENoCnt) ENoCnt) ENoCnt) ENoCnt
 
         describe "Evaluator.betaReduce" $ do
             it "Empty Expression" $ do
@@ -99,12 +107,33 @@ module EvaluatorSpec (spec) where
                 betaReduce 100 (getExpr "(x((lambda a.a)b)c)") `shouldBe`
                     Right (E (Id "x" (Id "b" (Id "c" ENoCnt))) ENoCnt)
 
-            it "Unnecessary parentheses" $ do
-                betaReduce 1000 (getExpr "(((lambda x.x)))") `shouldBe`
-                    Right (Fun ["x"] (Id "x" ENoCnt) ENoCnt)
+            it "Irreducible function inside Unnecessary parentheses" $ do
+                betaReduce 1000 (getExpr "(a((lambda x.x))c)") `shouldBe`
+                    Right (E (Id "a"(Fun ["x"] (Id "x" ENoCnt) (Id "c" ENoCnt))) ENoCnt)
+            
+            it "Reducible function inside uneccessary parentheses" $ do
+                betaReduce 1000 (getExpr "(((lambda x.x))a)") `shouldBe` 
+                    Right (Id "a" ENoCnt)
 
             it "Reach maximum evaluation" $ do
                 betaReduce 1000 (getExpr "(lambda f.((lambda x.(f (x x)))(lambda x.(f (x x)))))(lambda x.x)") `shouldSatisfy` isLeft
+
+            it "Irreducible Function that have reducible body" $ do
+                betaReduce 1000 (getExpr "(a(lambda x.(lambda y z.y z)b)c)") `shouldBe`
+                    Right(E (Id "a" (Fun ["x"] (Fun ["z"] (Id "b" (Id "z" ENoCnt)) ENoCnt) (Id "c" ENoCnt))) ENoCnt)
+
+            it "Reducible Function that have reducible body" $ do
+                betaReduce 1000 (getExpr "((lambda x.(lambda y z.y z)b)c)") `shouldBe`
+                    Right(Fun ["z"] (Id "b" (Id "z" ENoCnt)) ENoCnt)
+            
+            it "Function application with argument enclosed in parenthesis" $ do
+                betaReduce 100 (getExpr "((lambda x a.a x)(b c)d)") `shouldBe`
+                    Right (E (Id "d" (E (Id "b" (Id "c" ENoCnt)) ENoCnt)) ENoCnt) 
+            
+            it "Function application with body containing parentheses" $ do
+                betaReduce 100 (getExpr "((lambda x.x(x x)x)a)") `shouldBe`
+                    Right (E (Id "a" (E (Id "a" (Id "a" ENoCnt)) (Id "a" ENoCnt))) ENoCnt)
+                    
 
         describe "Evaluator.evaluate" $ do
             it "Evaluate expression; no error" $ do
@@ -125,4 +154,4 @@ module EvaluatorSpec (spec) where
 
             it "Evaluate declaration; variable already defined" $ do
                 evaluate 1000 (Map.fromList [("a", Id "b" ENoCnt)]) (Decl "a" (getExpr "(((lambda x.x)))")) `shouldBe`
-                    Left (VariableAlreadyDefined $ "a" ++ " is already defined as " ++ "b")
+                    Left VariableAlreadyDefined
