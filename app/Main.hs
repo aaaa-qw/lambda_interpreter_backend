ede029bd6a25cafce5e47f486c85b818ea317ce9
aaaa-qw <71070207+aaaa-qw@users.noreply.github.com>
2023-06-17 22:00:08 +0700

    Update Main.hs
    Use environmental variable for frontend url

--- app/Main.hs
+++ app/Main.hs
@@ -3,16 +3,22 @@ module Main (main) where
     import Network.Wai.Handler.Warp (run)
     import Network.Wai (Middleware)
     import WebService (app1)
-    import Network.Wai.Middleware.Cors    
+    import Network.Wai.Middleware.Cors
+    import System.Environment
+    
     main :: IO ()
-    main = run 8080 $ corsPolicy app1
+    main = do
+        mfrontend_url = lookupEnv "FRONTEND_URL"
+        case mfrontend_url of
+            Just url -> run 8080 $ (corsPolicy url) app1
+            Nothing  -> run 8080 % (corsPolicy "") app1
 
-    corsPolicy :: Middleware
-    corsPolicy = cors (const $ Just policy)
+    corsPolicy :: String -> Middleware
+    corsPolicy frontend_url = cors (const $ Just policy)
         where
             policy = simpleCorsResourcePolicy
                 { 
                     corsMethods = [ "GET", "POST", "PUT", "OPTIONS" ],
-                    corsOrigins = Just (["https://lambdainterpreterfrontend-production-42ea.up.railway.app", "http://localhost:3000"], True),
+                    corsOrigins = Just ([frontend_url, "http://localhost:3000"], True),
                     corsRequestHeaders = [ "Content-Type" ]
                 }
